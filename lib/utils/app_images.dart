5e347cc1dcb2cf9ec06792c5ad7e9bf88b007f37
lucas hafidi <assilajuen@gmail.com>
2025-11-19 12:39:16 +0100

    add images and fonts

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardBacground
+  /// assets/images/card_bacground.svg
+  static const String imagesCardBacground = "assets/images/card_bacground.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/Expenses.svg
+  static const String imagesExpenses = "assets/images/Expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyinvestement
+  /// assets/images/myinvestement.svg
+  static const String imagesMyinvestement = "assets/images/myinvestement.svg";
+
+  /// Assets for imagesMytransaction
+  /// assets/images/mytransaction.svg
+  static const String imagesMytransaction = "assets/images/mytransaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistic
+  /// assets/images/statistic.svg
+  static const String imagesStatistic = "assets/images/statistic.svg";
+
+  /// Assets for imagesWalletaccount
+  /// assets/images/walletaccount.svg
+  static const String imagesWalletaccount = "assets/images/walletaccount.svg";
+}
+
