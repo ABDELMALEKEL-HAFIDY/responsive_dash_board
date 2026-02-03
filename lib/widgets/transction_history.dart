a028d955d241faa8d17958cd7bcf2c5ab970424f
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-02-03 07:24:34 +0100

    transction history part 2

--- lib/widgets/transction_history.dart
+++ lib/widgets/transction_history.dart
@@ -7,14 +7,35 @@ class TransctionHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Transction History", style: AppStyles.styleSemiBold20),
-            Text('See All', style: AppStyles.styleMedium16.copyWith(color: Color(0xFF4EB7F2)),),
-          ],
+        TransctionHistoryHeader(),
+        const SizedBox(height: 20),
+        Text('13 April 2025 ',
+        style: TextStyle(
+          color: Color(0xFFAAAAAA),
+          fontSize: 16,
+          fontFamily: 'Montserrat',
+          fontWeight: FontWeight.w500
+        ),
         )
+      ],
+    );
+  }
+}
+
+class TransctionHistoryHeader extends StatelessWidget {
+  const TransctionHistoryHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("Transction History", style: AppStyles.styleSemiBold20),
+        Text('See All', style: AppStyles.styleMedium16.copyWith(color: Color(0xFF4EB7F2)),),
       ],
     );
   }
