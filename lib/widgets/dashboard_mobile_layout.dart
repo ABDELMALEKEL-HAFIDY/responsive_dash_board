a77153160535e2733990edd11f1583057593cb30
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-02-12 11:28:37 +0100

    tablet layout part two and create lyout for mobile

---
+++ lib/widgets/dashboard_mobile_layout.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice_section.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transction_history_section.dart';
+
+class DashboardMobileLayout extends StatelessWidget {
+  const DashboardMobileLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AllExpensessAndQuickInvoiceSection(),
+          SizedBox(height: 24,),
+          MyCardAndTransctionHistorySection(),
+          SizedBox(height: 24,),
+          IncomeSection(),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transction_history_section.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -11,9 +14,10 @@ class DashboardTabletLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(flex: 3, child: AllExpensessAndQuickInvoiceSection()),
+        Expanded(flex: 3, child: DashboardMobileLayout()),
         SizedBox(width: 32,)
       ],
     );
   }
 }
+
