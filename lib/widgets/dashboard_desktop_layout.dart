c7156489dba022e1227bc3e5790a22f15b6a1cdb
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-02-13 11:23:00 +0100

    responsive desktop layout part one

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -16,18 +16,35 @@ class DashboardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(flex: 2, child: Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: AllExpensessAndQuickInvoiceSection(),
-        )),
-        SizedBox(width: 24),
         Expanded(
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              MyCardAndTransctionHistorySection(),
-              SizedBox(height: 24),
-              Expanded(child: IncomeSection()),
+          flex: 3,
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 40),
+                        child: AllExpensessAndQuickInvoiceSection(),
+                      ),
+                    ),
+                    SizedBox(width: 24),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 40),
+                          MyCardAndTransctionHistorySection(),
+                          SizedBox(height: 24),
+                          Expanded(child: IncomeSection()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
