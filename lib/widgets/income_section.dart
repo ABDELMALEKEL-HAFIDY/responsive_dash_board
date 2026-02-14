3531a7180eb6d2ed01276bd8fa91a0f3c26d29d0
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-02-14 11:50:45 +0100

    responsive desktop layout part five condition

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_details.dart';
+import 'package:responsive_dash_board/widgets/income_section_body.dart';
 import 'package:responsive_dash_board/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -14,17 +15,13 @@ class IncomeSection extends StatelessWidget {
       child: Column(
         children: [
         IncomeSectionHeader(),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(child: IncomeChart()),
-            Expanded(flex: 2,child: IncomeDetails())
-          ],
-        )
+        IncomeSectionBody()
       ],
     ));
   }
 }
+
+
 
 
 
